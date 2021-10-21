19fd4124c85f9303eb56704b9f4ff993552464aa
Dickeo <bryan.amaya4242@gmail.com>
2021-10-20 23:29:53 -0600

    Primer commit

---
+++ sockets/controller.js
@@ -0,0 +1,22 @@
+
+const socketController = (socket) => {
+    console.log('Client online', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client offline', socket.id);
+    })
+
+    socket.on('send-message', (payload, callback) => {
+        
+        const id = 123456;
+        callback(id);
+
+        socket.broadcast.emit('send-message', payload);
+
+    });
+}
+
+module.exports = {
+    socketController,
+
+}
